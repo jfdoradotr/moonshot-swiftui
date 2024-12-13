76a2f7f423b18ac7833e51fb90fd815412795cd9
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 17:01:14 -0600

    Add the `Mission` required property and inject it on the Preview

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -5,11 +5,16 @@
 import SwiftUI
 
 struct MissionView: View {
+  let mission: Mission
+
   var body: some View {
     Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
   }
 }
 
 #Preview {
-  MissionView()
+  let missions: [Mission] = Bundle.main.decode("missions.json")
+
+  return MissionView(mission: missions[0])
+    .preferredColorScheme(.dark)
 }
