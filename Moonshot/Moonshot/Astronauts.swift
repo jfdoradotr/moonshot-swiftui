9491123a4568eba15f9623a4894a7781247cd062
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 09:45:11 -0600

    Create the Astronaut model

---
+++ Moonshot/Moonshot/Astronauts.swift
@@ -0,0 +1,11 @@
+//	
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
