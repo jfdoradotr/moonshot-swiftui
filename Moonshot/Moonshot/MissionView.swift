2468295820d26238790827ca7e18c3da9d25419a
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 16:59:12 -0600

    Create MissionView file

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  MissionView()
+}
