d7ff4acfdb1d347987f545265e69f189fa88dc85
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 17:04:44 -0600

    Create the mission view

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -8,13 +8,35 @@ struct MissionView: View {
   let mission: Mission
 
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ScrollView {
+      VStack {
+        Image(mission.image)
+          .resizable()
+          .scaledToFit()
+          .containerRelativeFrame(.horizontal) { width, axis in
+            width * 0.6
+          }
+          .padding(.top)
+
+        VStack(alignment: .leading) {
+          Text("Mission Highlights")
+            .font(.title.bold())
+            .padding(.bottom, 5)
+          Text(mission.description)
+        }
+        .padding(.horizontal)
+      }
+      .padding(.bottom)
+    }
+    .navigationTitle(mission.displayName)
+    .navigationBarTitleDisplayMode(.inline)
+    .background(.darkBackground)
   }
 }
 
 #Preview {
   let missions: [Mission] = Bundle.main.decode("missions.json")
 
-  return MissionView(mission: missions[0])
+  return NavigationStack { MissionView(mission: missions[0]) }
     .preferredColorScheme(.dark)
 }
