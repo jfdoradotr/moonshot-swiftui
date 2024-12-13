827c5c2410ffa96a18bf7ef662518359a527dfe0
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 17:51:03 -0600

    Present the Astronaut details

--- Moonshot/Moonshot/AstronautView.swift
+++ Moonshot/Moonshot/AstronautView.swift
@@ -2,4 +2,30 @@
 // Copyright © Juan Francisco Dorado Torres. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct AstronautView: View {
+  let astronaut: Astronaut
+
+  var body: some View {
+    ScrollView {
+      VStack {
+        Image(astronaut.id)
+          .resizable()
+          .scaledToFit()
+        Text(astronaut.description)
+          .padding()
+      }
+      .background(.darkBackground)
+      .navigationTitle(astronaut.name)
+      .navigationBarTitleDisplayMode(.inline)
+    }
+  }
+}
+
+#Preview {
+  let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+  return AstronautView(astronaut: astronauts["aldrin"]!)
+    .preferredColorScheme(.dark)
+}
