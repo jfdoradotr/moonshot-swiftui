a0b1068885c2e4a65ae2fbd068ec57ff9a217cf1
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 17:16:05 -0600

    Create the carrousel with the crew members

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -42,6 +42,37 @@ struct MissionView: View {
           Text(mission.description)
         }
         .padding(.horizontal)
+
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack {
+            ForEach(crew, id: \.role) { crewMember in
+              NavigationLink {
+                Text("Astronaut details")
+              } label: {
+                HStack {
+                  Image(crewMember.astronaut.id)
+                    .resizable()
+                    .frame(width: 104, height: 72)
+                    .clipShape(.capsule)
+                    .overlay(
+                      Capsule()
+                        .strokeBorder(.white, lineWidth: 1)
+                    )
+
+                  VStack(alignment: .leading) {
+                    Text(crewMember.astronaut.name)
+                      .foregroundStyle(.white)
+                      .font(.headline)
+
+                    Text(crewMember.role)
+                      .foregroundStyle(.secondary)
+                  }
+                }
+                .padding(.horizontal)
+              }
+            }
+          }
+        }
       }
       .padding(.bottom)
     }
