48f491f3cce92835afaa45b531d6bd19f4660d16
Juan Dorado <jfdoradotr@icloud.com>
2024-12-13 18:25:18 -0600

    Add a list/grid option view in the toolbar

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,41 +8,64 @@ struct ContentView: View {
   let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
   let missions: [Mission] = Bundle.main.decode("missions.json")
 
-  let columns = [
-    GridItem(.adaptive(minimum: 150))
-  ]
+  @State private var isShowingGrid = true
+
+
+
+  private var gridLayout: some View {
+    let columns = [GridItem(.adaptive(minimum: 150))]
+
+    return LazyVGrid(columns: columns) {
+      missionContentItems
+    }
+  }
+
+  private var listLayout: some View {
+    LazyVStack {
+      missionContentItems
+    }
+  }
+
+  private var missionContentItems: some View {
+    ForEach(missions) { mission in
+      NavigationLink {
+        MissionView(mission: mission, astronauts: astronauts)
+      } label: {
+        VStack {
+          Image(mission.image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 100, height: 100)
+            .padding(.vertical)
+          VStack {
+            Text(mission.displayName)
+              .font(.headline)
+              .foregroundStyle(.white)
+            Text(mission.formattedLaunchDate)
+              .font(.caption)
+              .foregroundStyle(.white.opacity(0.5))
+          }
+          .padding(.vertical)
+          .frame(maxWidth: .infinity)
+          .background(.lightBackground)
+        }
+        .clipShape(.rect(cornerRadius: 10))
+        .overlay(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(.lightBackground)
+        )
+      }
+    }
+  }
 
   var body: some View {
     NavigationStack {
       ScrollView {
-        LazyVGrid(columns: columns) {
-          ForEach(missions) { mission in
-            NavigationLink {
-              MissionView(mission: mission, astronauts: astronauts)
-            } label: {
-              VStack {
-                Image(mission.image)
-                  .resizable()
-                  .scaledToFit()
-                  .frame(width: 100, height: 100)
-                VStack {
-                  Text(mission.displayName)
-                    .font(.headline)
-                    .foregroundStyle(.white)
-                  Text(mission.formattedLaunchDate)
-                    .font(.caption)
-                    .foregroundStyle(.white.opacity(0.5))
-                }
-                .padding(.vertical)
-                .frame(maxWidth: .infinity)
-                .background(.lightBackground)
-              }
-              .clipShape(.rect(cornerRadius: 10))
-              .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                  .stroke(.lightBackground)
-              )
-            }
+        Group {
+          if isShowingGrid {
+            gridLayout
+          } else {
+            listLayout
           }
         }
         .padding([.horizontal, .bottom])
@@ -50,6 +73,18 @@ struct ContentView: View {
       .navigationTitle("Moonshot")
       .background(.darkBackground)
       .preferredColorScheme(.dark)
+      .toolbar {
+        ToolbarItem {
+          Button(
+            isShowingGrid ? "Grid" : "List",
+            systemImage: isShowingGrid ? "square.fill.text.grid.1x2" : "square.grid.2x2"
+          ) {
+            withAnimation {
+              isShowingGrid.toggle()
+            }
+          }
+        }
+      }
     }
   }
 }
